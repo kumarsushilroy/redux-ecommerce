c880c93eaf464ef94cbb0a391ba90b566e66b484
Sushil Kumar <100270367+kumarsushilroy@users.noreply.github.com>
2023-09-27 21:04:51 +0530

    forth commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
       <div className='mt-5'>
         <Category/>
         <p className='text-red-600'>hello</p>
-        <p className='text-green-500'>kya hai</p>
+        <p className='text-green-500'>kya hai be</p>
       </div>
     </div>
   )
